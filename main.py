31a83164ed789cb80c08a3af2ffa30c2042e86a0
isaac <isaacbmckee@gmail.com>
2023-09-14 10:54:12 -0500

    testing way for user to input their wanted filter

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from PIL import Image, ImageFilter
+import importlib
  
-# creating a image object
+#getting image
 imagetoedit = input("Path to Image: ")
 im1 = Image.open(f"{imagetoedit}")
 
 #filters
 im2 = im1.filter(ImageFilter.BLUR)
- 
-im2.show()
+im3 = im1.filter(ImageFilter.CONTOUR)
+im4 = im1.filter(ImageFilter.DETAIL)
+im5 = im1.filter(ImageFilter.EDGE_ENHANCE_MORE)
+im6 = im1.filter(ImageFilter.EMBOSS)
+im7 = im1.filter(ImageFilter.FIND_EDGES)
+im8 = im1.filter(ImageFilter.SHARPEN)
+im9 = im1.filter(ImageFilter.SMOOTH)
+im10 = im1.filter(ImageFilter.SMOOTH_MORE)
+
+filtertoapply = input("Filter: ")
+if filtertoapply == "im2":
+    im2.show()
+if filtertoapply == "im3":
+    im3.show()
+if filtertoapply == "im4":
+    im4.show()
+if filtertoapply == "im5":
+    im5.show()
+if filtertoapply == "im6":
+    im6.show()
+if filtertoapply == "im7":
+    im7.show()
